f907b746bc2d2d4f540e92c07536cdc70ed0f444
valencianok <valenciano.k@gmail.com>
2016-06-15 01:06:40 -0600

    Basic GetMessage funcion

---
+++ Wit.ai.net.Tests/WitClient_Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.valgut.libs.bots.Wit;
+
+namespace Wit.ai.net.Tests
+{
+    [TestClass]
+    public class WitClient_Tests
+    {
+        string token = "<your_wit_token>";
+
+        [TestMethod]
+        public void WitClient_GetMessage_Test()
+        {
+            var client = new WitClient(token);
+            var message = client.GetMessage("hello");
+
+            Assert.IsNull(message.error);
+        }
+    }
+}
---
+++ Wit.ai.net/Models/Entity.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.valgut.libs.bots.Wit.Models
+{
+    public class Entity
+    {
+        public string metadata { get; set; }
+
+        public JToken value{get;set;}
+
+        public double confidence { get; set; }
+    }
+}
