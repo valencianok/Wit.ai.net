dc758f10910bf67b762a1f0dc35759393ba3f98b
macekkrystof <macek.krystof@outlook.cz>
2017-11-12 16:37:49 +0100

    added correct project

---
+++ WitAi.NetStandard20/Models/Entity.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WitAi.NetStandard20.Models
+{
+    public class Entity
+    {
+        public string metadata { get; set; }
+
+        public JToken value{get;set;}
+
+        public double confidence { get; set; }
+    }
+}
